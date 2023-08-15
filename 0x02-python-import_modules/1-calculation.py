ff0ad4d662e6ad7b9c08bef04e880357cbb01f8d
AziMadolo <aziziphomadolo@gmail.com>
2023-08-15 15:53:17 -0700

    1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,16 +1,37 @@
 #!/usr/bin/python3
 
-from calculator_1 import add, sub, mul, div
+def add(a, b):
+    """Perform addition: a + b"""
+    return a + b
 
-def main():
-    """Print the sum, difference, product, and quotient of 10 and 5."""
-    a = 10
-    b = 5
+def sub(a, b):
+    """Perform subtraction: a - b"""
+    return a - b
 
-    print(f"{a} + {b} = {add(a, b)}")
-    print(f"{a} - {b} = {sub(a, b)}")
-    print(f"{a} * {b} = {mul(a, b)}")
-    print(f"{a} / {b} = {div(a, b)}")
+def mul(a, b):
+    """Perform multiplication: a * b"""
+    return a * b
+
+def div(a, b):
+    """Perform division: a / b"""
+    return int(a / b)
+def perform_calculations(a, b):
+    """Perform addition, subtraction, multiplication, and division on two numbers."""
+    results = {
+        "sum": add(a, b),
+        "difference": sub(a, b),
+        "product": mul(a, b),
+        "quotient": div(a, b)
+    }
+    return results
 
 if __name__ == "__main__":
-    main()
+    num1 = 10
+    num2 = 5
+
+    calculations = perform_calculations(num1, num2)
+
+    print(f"{num1} + {num2} = {calculations['sum']}")
+    print(f"{num1} - {num2} = {calculations['difference']}")
+    print(f"{num1} * {num2} = {calculations['product']}")
+    print(f"{num1} / {num2} = {calculations['quotient']}")
