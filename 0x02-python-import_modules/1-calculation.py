21964ad7ac203aa7982ce5cc1428cb5f29547f13
AziMadolo <aziziphomadolo@gmail.com>
2023-08-15 15:50:26 -0700

    1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,70 +1,16 @@
 #!/usr/bin/python3
 
-def add(a, b):
-    """My addition function
+from calculator_1 import add, sub, mul, div
 
-    Args:
-        a: first integer
-        b: second integer
+def main():
+    """Print the sum, difference, product, and quotient of 10 and 5."""
+    a = 10
+    b = 5
 
-    Returns:
-        The return value. a + b
-    """
-    return (a + b)
-
-def sub(a, b):
-    """My subtraction function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a - b
-    """
-    return (a - b)
-
-def mul(a, b):
-    """My multiplication function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a * b
-    """
-    return (a * b)
-
-def div(a, b):
-    """My division function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a / b
-    """
-    return int(a / b)
-
-def perform_calculations(a, b):
-    """Perform addition, subtraction, multiplication, and division on two numbers."""
-    results = {
-        "sum": add(a, b),
-        "difference": sub(a, b),
-        "product": mul(a, b),
-        "quotient": div(a, b)
-    }
-    return results
+    print(f"{a} + {b} = {add(a, b)}")
+    print(f"{a} - {b} = {sub(a, b)}")
+    print(f"{a} * {b} = {mul(a, b)}")
+    print(f"{a} / {b} = {div(a, b)}")
 
 if __name__ == "__main__":
-    num1 = 10
-    num2 = 5
-
-    calculations = perform_calculations(num1, num2)
-
-    print("{} + {} = {}".format(num1, num2, calculations["sum"]))
-    print("{} - {} = {}".format(num1, num2, calculations["difference"]))
-    print("{} * {} = {}".format(num1, num2, calculations["product"]))
-    print("{} / {} = {}".format(num1, num2, calculations["quotient"]))
+    main()
