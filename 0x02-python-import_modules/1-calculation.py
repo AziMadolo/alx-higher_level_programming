a22760d814bc2e8fceb549669932e4b4d09d90e0
AziMadolo <aziziphomadolo@gmail.com>
2023-08-15 15:40:01 -0700

    1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,16 +1,61 @@
 #!/usr/bin/python3
 
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
+
+def sub(a, b):
+    """My subtraction function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a - b
+    """
+    return (a - b)
+
+def mul(a, b):
+    """My multiplication function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a * b
+    """
+    return (a * b)
+
+def div(a, b):
+    """My division function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a / b
+    """
+    return int(a / b)
+
 def perform_calculations(a, b):
     """Perform addition, subtraction, multiplication, and division on two numbers."""
-    from calculator_1 import add, sub, mul, div
-    
     results = {
         "sum": add(a, b),
         "difference": sub(a, b),
         "product": mul(a, b),
         "quotient": div(a, b)
     }
-    
     return results
 
 if __name__ == "__main__":
